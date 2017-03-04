dab5ad635fd1c0de61e445207269ac0a7d674571
Jin Lim <jinweilim22@hotmail.com>
2017-03-04 03:53:35 +0000

    Add files via upload

--- non_tf_imgprocess/mosaic_crop.py
+++ non_tf_imgprocess/mosaic_crop.py
@@ -13,7 +13,7 @@ increase from top to bottom of the mosaic.
 Parameters to define are: 
 excelfile - name (with path) of csvfile
 mosfile - name (with path) of mosaic file
-imgsize - dimensions of desired cropped image (eg. if 24x24 pixels, imgsize = 24)
+boxsize - dimensions of desired cropped image (eg. if 24x24 pixels, boxsize = 24)
 imgname - naming of the item of interest (without .tif) #update!
 path - path of folder to save cropped images
 trainingtest - ratio of train to test
@@ -35,6 +35,9 @@ import csv
 from PIL import Image
 import random
 import numpy
+#import copy
+#import time
+import sys
 
 Image.MAX_IMAGE_PIXELS = 1000000000 #to silence DecompressionBombWarning DOS
 
@@ -48,6 +51,7 @@ BPl = ["Bathymodiolus_platifrons_corrected", "BPl", "Bathymodiolus_platifrons", 
 Par = ["Paralomis", "Par", "Paralomis", 4]
 SCr = ["Shinkaia_crosnieri", "SCr", "Shinkaia_crosnieri", 2]
 TDe = ["Thermosipho_desbruyesi", "TDe","Thermosipho_desbruyesi", 5]
+NSe = ["Null_set", "NSe", "Null_set", 6]
 
 """to automate run all species"""
 #x = [Alv, BJa, BPl, Par, SCr, TDe]
@@ -55,13 +59,14 @@ TDe = ["Thermosipho_desbruyesi", "TDe","Thermosipho_desbruyesi", 5]
 #    species = i
 #    training(trainingtest)
 
+allspecies = [Alv, BJa, BPl, Par, SCr, TDe]
 species = SCr
 
 excelfile = "{}.csv".format(species[0])
 mosfile = "D:/Iheya_n/NBC_2m_2014/mosaics/NBC_2m_2014_mosaic_layer.tif"
-imgsize = 50
+boxsize = 50  #must be even number, boxsize of 50 means 50x50pixel box
 imgname = species[1]
-path = "D:/Iheya_n/NBC/{}/{}_{}".format(species[2], species[1], imgsize)
+path = "D:/Iheya_n/NBC/{}/{}_{}".format(species[2], species[1], boxsize)
 """check if cifar code already included training and testing sample"""
 trainingtest = 3/1
 totalnumber = 7000 #"""number of each species ie. test + training"""
@@ -75,24 +80,143 @@ def count():
             m = m+1
     return m
 
-#crops and saves all the sample images in a species
-def mosaic_crop(excelfile, mosfile, imgsize, imgname): 
+#crops and saves all the sample images in a species ***can save space by directly converting to array and bin file, just csv file for reference instead of saving the images.
+def mosaic_crop(excelfile, mosfile, boxsize, imgname): 
     img = Image.open(mosfile)
-    with open (excelfile) as csvfile:
+    with open(excelfile) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         n = 0
         for row in readCSV:
             img1 = img.crop(
                     (
-                            float(row[0])-imgsize/2,
-                            float(row[1])-imgsize/2,
-                            float(row[0])+imgsize/2,
-                            float(row[1])+imgsize/2
+                            float(row[0])-boxsize/2,
+                            float(row[1])-boxsize/2,
+                            float(row[0])+boxsize/2,
+                            float(row[1])+boxsize/2
                     )
             )
             n = n+1
             img1.save(path + "/" + imgname + "_{}_all/{}-{}.tif".\
-                      format(imgsize, imgname, "%05d" % n))
+                      format(boxsize, imgname, "%05d" % n))
+
+#20320x20320 pixels, include line if array all white (ie [255,255,255]) or more than 75% white then delete it
+#for 50x50 pixel of whole 20320x20320 map, 20 pixel at the right end and btm end neglected
+#generate random number and filter, and ... 
+def null_set():
+    nullpath = path[:14]+"/{}/{}_{}".format(NSe[0],NSe[1],boxsize)
+    nullList = []
+    img = Image.open(mosfile)
+#    pixel = [-boxsize/2, -boxsize/2]
+    numberofhorizontalrows = int(img.size[0]/boxsize)
+    numberofverticalcolumns = int(img.size[1]/boxsize)
+#    imgwithallspecies0 = []
+#    for i in range(numberofhorizontalrows*50): #took too long to generate 412090000 coordinates
+#        for j in range(numberofverticalcolumns*50):
+#            pixel = [1 + i*1, 1 + i*j]
+#            imgwithallspecies0.append(pixel)
+    for j in range(numberofhorizontalrows):
+        for i in range(numberofverticalcolumns):
+            pixel = [boxsize/2 + i*boxsize, boxsize/2 + j*boxsize]
+            nullList.append(pixel)
+    allspeciescoord = []
+    for i in allspecies:
+        species = i
+        excelfile = "{}.csv".format(species[0])
+        with open(excelfile) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=",")
+            for row in readCSV:
+                coord = [float(row[0]),float(row[1])]
+                allspeciescoord.append(coord)
+    randomnullListtemp = random.sample(range(0,len(nullList)), len(nullList))
+#    randomnullListtemp = copy.copy(nullList)
+    randomnullListtemp2 = []
+    r = -1
+    n = 0
+    while len(randomnullListtemp2) < totalnumber: #problem: is the first 7000 of the random sample 164836 truly randomly distributed?
+        r = r + 1
+        rndmcoord = nullList[randomnullListtemp[r]]
+        for i in allspeciescoord:
+            if abs(rndmcoord[0]-i[0]) < boxsize + 25:
+                if abs(rndmcoord[1]-i[1]) < boxsize + 25:
+                    break
+                else:
+                    pass
+            else:
+                
+#    print ("First filtered number of random samples is {}".format\
+#           (len(randomnullListtemp2)))
+                    img1 = img.crop(
+                            (
+                                    rndmcoord[0]-boxsize/2,
+                                    rndmcoord[1]-boxsize/2,
+                                    rndmcoord[0]-boxsize/2,
+                                    rndmcoord[1]-boxsize/2
+                            )
+                    )
+                    Iar = numpy.asarray(img1)
+                    Iar2 = (Iar.tolist())
+                    print (type(Iar))
+                    print (type(Iar2))
+#                    Iar2 = Iar.tolist()
+#                    print (type(Iar2))
+                    sys.exit("asdasd")
+#                    numberofwhitepixels = 0
+#                    for row in Iar2:
+#                        numberofwhitepixels = numberofwhitepixels + \
+#                        row.count([255, 255, 255])
+#                    if numberofwhitepixels/boxsize**2 < 0.25:
+#                        randomnullListtemp2.append(rndmcoord)
+#                        break
+#                    else:
+#                        break
+#    print (len(randomnullListtemp2))
+#    sys.exit("stop here")
+#                        n = n + 1
+#                        img1.save(nullpath+"/{}_{}_all/{}-{}.tif".format\
+#                                  (NSe[1],boxsize,NSe[1],"%06d"%n))
+#    nullArray = numpy.asarray(randomnullListtemp2)
+#    numpy.savetxt(nullpath+"/{}_{}.csv".format(NSe[0],boxsize),\
+#                      nullArray, delimiter=",")
+
+
+
+
+#    for i in nullList: #this step is taking too long!
+#        for j in allspeciescoord:
+#            if abs(i[0]-j[0]) < boxsize + 25: #assuming all creatures lies within boxsize + 25 pixels
+##            if i[0]-boxsize/2<= j[0]<= i[0]+boxsize/2:
+#                if abs(i[1]-j[1]) < boxsize + 25:
+##                if i[1]-boxsize/2<= j[1]<= i[1]+boxsize/2:
+#                    nullList.remove(i)
+#        n = 0
+#        for j in nullList:
+#            img1 = img.crop(
+#                    (
+#                            j[0]-boxsize/2,
+#                            j[1]-boxsize/2,
+#                            j[0]+boxsize/2,
+#                            j[1]+boxsize/2
+#                    )
+#            )
+#            Iar = numpy.asarray(img1)
+#            Iar2 = Iar.tolist()
+#            numberofwhitepixels = 0
+#            for row in Iar2:
+#                numberofwhitepixels = numberofwhitepixels + \
+#                row.count([255, 255, 255])
+#            if numberofwhitepixels/boxsize**2 > 0.25:
+#                nullList.remove(j)
+#            else:
+#                n = n+1
+#                img1.save(nullpath+"/{}_{}_all/{}-{}.tif".format(NSe[1],\
+#                          boxsize,NSe[1],"%06d"%n))
+#    nullArray = numpy.asarray(nullList)
+#    numpy.savetxt(nullpath+"/{}_{}.csv".format(NSe[0],boxsize),\
+#                      nullArray, delimiter=",")
+
+#        path = "D:/Iheya_n/NBC/{}/{}_{}".format(species[2], species[1], boxsize)
+#        readtxt = open(path + "/{}{}_{}.txt".format(species[1], 'test', boxsize), "r")
+#        string = string + readtxt.read()
 
 #generate training and test set, training and test csv file
 def training(trainingtest):
@@ -109,13 +233,12 @@ def training(trainingtest):
             j=j+1
     numpy.savetxt(path + "/" + imgname + "train.csv", trainingset, delimiter=",")
     numpy.savetxt(path + "/" + imgname + "test.csv", testset, delimiter=",")
-
 #def convertImg():
-    ImgArrayTrain = open(path + "/{}train_{}.txt".format(species[1], imgsize), "a")
-    ImgArrayTest = open(path + "/{}test_{}.txt".format(species[1], imgsize), "a")
+    ImgArrayTrain = open(path + "/{}train_{}.txt".format(species[1], boxsize), "a")
+    ImgArrayTest = open(path + "/{}test_{}.txt".format(species[1], boxsize), "a")
     for i in trainingset:
         Io = Image.open(path + "/" + imgname + "_{}_all/{}-{}.tif".\
-                        format(imgsize, imgname, "%05d" % i)) #"%05d" % n)
+                        format(boxsize, imgname, "%05d" % i)) #"%05d" % n)
         Iar = numpy.asarray(Io)
         Iar1 = "{},".format(species[3])
         for row in Iar:
@@ -134,7 +257,7 @@ def training(trainingtest):
         ImgArrayTrain.write(Iar1)
     for i in testset:
         Io = Image.open(path + "/" + imgname + "_{}_all/{}-{}.tif".\
-                        format(imgsize, imgname, "%05d" % i)) #"%05d" % n)
+                        format(boxsize, imgname, "%05d" % i)) #"%05d" % n)
         Iar = numpy.asarray(Io)
         Iar1 = "{},".format(species[3])
         for row in Iar:
@@ -157,11 +280,11 @@ def training(trainingtest):
 def txttobinconverter():
     testortrain = ['train'] #, 'test']
     for i in testortrain:
-        readtxt = open(path + "/{}{}_{}.txt".format(species[1], i, imgsize), "r")
+        readtxt = open(path + "/{}{}_{}.txt".format(species[1], i, boxsize), "r")
         string = readtxt.read()
         stringtolist = [int(s) for s in string[:-1].split(",")]
         buffer = bytes(stringtolist)
-        strfile = r"{}/{}{}_{}.bin".format(path, species[1], i, imgsize)
+        strfile = r"{}/{}{}_{}.bin".format(path, species[1], i, boxsize)
         with open(strfile, 'bw') as f:
             f.write(buffer)
 
@@ -170,16 +293,16 @@ def txttobinconvertertestcombine():
     string = ""
     for i in selectedspecies:
         species = i
-        path = "D:/Iheya_n/NBC/{}/{}_{}".format(species[2], species[1], imgsize)
-        readtxt = open(path + "/{}{}_{}.txt".format(species[1], 'test', imgsize), "r")
+        path = "D:/Iheya_n/NBC/{}/{}_{}".format(species[2], species[1], boxsize)
+        readtxt = open(path + "/{}{}_{}.txt".format(species[1], 'test', boxsize), "r")
         string = string + readtxt.read()
     stringtolist = [int(s) for s in string[:-1].split(",")]
     buffer = bytes(stringtolist)
-    strfile = r"{}/{}{}_{}.bin".format(path[:10], "BJaBPlSCr", 'test', imgsize)
+    strfile = r"{}/{}{}_{}.bin".format(path[:10], "BJaBPlSCr", 'test', boxsize)
     with open(strfile, 'bw') as f:
         f.write(buffer)
 
-#mosaic_crop(excelfile, mosfile, imgsize, imgname)
+#mosaic_crop(excelfile, mosfile, boxsize, imgname)
 #training(trainingtest)
 #txttobinconverter()
 
